a88a889d8d4c4a5781d4f3e8d9a824780ccf89e3
Feather <liangzhi21@qq.com>
2020-11-12 17:43:52 +0800

    模板解析

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -35,8 +35,31 @@ function gen(node) {
     return generate(node);
   } else {
     // 文本
-    let text = node.text;
-    return `_v(${text})`;
+    let text = node.text; // xxx {{name}} xxx {{age}} => 'xxx' + _s(name) + 'xxx' + _s(age); 
+    // 存放匹配的结果
+    let tokens = [];
+    // 匹配到的值，索引
+    let match, index;
+    // 索引
+    let lastIndex = defaultTagRE.lastIndex = 0;
+    while (match = defaultTagRE.exec(text)) {
+      // 当前匹配到的位置
+      index = match.index;
+      // 如果当前截取到的位置大于正则索引位置则把匹配到的那一段数据放到tokens里面
+      if (index > lastIndex) {
+        tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+      }
+      // 匹配到{{age}}的时候把ags放到`_s()`的方法里面
+      tokens.push(`_s(${match[1].trim()})`);
+      // 然后把当前的正则匹配索引置 为  当前匹配的位置+截取的数据的长度，也就是下一次开始的位置
+      lastIndex = index + match[0].length;
+    }
+    // 如果最后还有剩余，则push到最后面。
+    if (lastIndex < text.length) {
+      tokens.push(JSON.stringify(text.slice(lastIndex)));
+    }
+    // 用`+`号连接
+    return `_v(${tokens.join('+')})`;
   }
 }
 
